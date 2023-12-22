1fc447a055a9e651e174e91c15d018ad62b9d47f
Luka Grabnar <grabnar.luka@gmail.com>
2023-12-22 23:34:01 +0100

    useAction optimization

--- src/components/hooks/use-action.ts
+++ src/components/hooks/use-action.ts
@@ -1,9 +1,12 @@
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../../states";
+import { useMemo } from "react";
 
 export const useAction = () => {
     const dispatch = useDispatch();
 
-    return bindActionCreators(actionCreators, dispatch);
+    return useMemo(() => {
+        return bindActionCreators(actionCreators, dispatch);
+    }, [dispatch]);
 }
