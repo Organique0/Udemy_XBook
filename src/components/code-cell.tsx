a47490aad4b04f8b94af02abc805af3d8ce7a68d
Luka Grabnar <grabnar.luka@gmail.com>
2023-12-22 23:36:43 +0100

    improved code-cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,15 +1,16 @@
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import CodeEditor from './code-editor';
-import bundle from '../bundler';
 import Resizable from "./resizable";
-//for some reason this is super important for jsx syntax highlighting 
-//why? why? why? why?
+
 import { Buffer } from 'buffer';
 import Preview from './preview';
 import { Cell } from "../states";
 import { useAction } from "./hooks/use-action";
-// @ts-expect-error okokok
+import { useTypedSelector } from "./hooks/use-typed-selector";
+import '../styles/code-cell.css';
+
+//important for jsx syntax highlighting
 window.Buffer = Buffer;
 
 interface CodeCellProps {
@@ -17,22 +18,23 @@ interface CodeCellProps {
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-    const [code, setCode] = useState('');
-    const [err, setErr] = useState('');
-
-    const { updateCell } = useAction();
+    const { updateCell, createBundle } = useAction();
+    const bundle = useTypedSelector(state => state.bundles[cell.id]);
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle(cell.id, cell.content);
+            return;
+        }
         const timer = setTimeout(async () => {
-            const output = await bundle(cell.content);
-            setCode(output.code);
-            setErr(output.err);
+            createBundle(cell.id, cell.content);
         }, 1000);
 
         return () => {
             clearTimeout(timer);
         }
-    }, [cell.content]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cell.content, cell.id, createBundle]);
 
     //my solution for changing element on resizing
     //using css is probable a better idea. mine was a bit laggy at times
@@ -53,7 +55,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)} />
                 </Resizable>
                 {/* {!resizing ? <Preview code={code} /> : <div>hello</div>} */}
-                {<Preview code={code} bundleError={err} />}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress className="progress is-small is-primary" max="100">Loading</progress>
+                        </div>
+                    ) : <Preview code={bundle.code} bundleError={bundle.err} />}
+                </div>
             </div>
         </Resizable>
     )
