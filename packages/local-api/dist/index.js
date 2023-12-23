fef1ca113641220de17e0c74d0056ed86f5dfe42
Luka Grabnar <grabnar.luka@gmail.com>
2023-12-23 21:49:17 +0100

    first steps of creating a cli

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log("server is listening");
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log("serving trafic on port", port);
+    console.log("serving/fetching cells form ", filename);
+    console.log("that file is in", dir);
 };
+exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log("server is listening")
+export const serve = (port: number, filename: string, dir: string) => {
+    console.log("serving trafic on port", port);
+    console.log("serving/fetching cells form ", filename);
+    console.log("that file is in", dir);
 }
